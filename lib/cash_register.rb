ca6e4a75c0a77fe783eef29bfcae545542d3d9bc
Melissa Fields <melissamfields12@gmail.com>
2023-03-29 12:46:47 -0700

    commit tree 7d2cb17522cc079812b2b60f5e72aa4e1798379f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,41 @@
+class CashRegister
+    attr_accessor :total, :last_item
+    attr_reader :discount, :items
+   
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_item = 0
+    end
+
+    def add_item title, price, quantity = 1
+        quantity.times do 
+            self.items << title
+    end
+        self.last_item = price * quantity
+        self.total += price * quantity
+end
+
+    def apply_discount
+        if discount > 0
+            @total -= @total * (@discount / 100.0)
+            "After the discount, the total comes to $#{total.to_i}."
+        else
+            "There is no discount to apply."
+    end
+end
+
+    def items
+        @items
+    end
+
+
+
+    def void_last_transaction
+        @total -= @last_item
+    end
+
+end
 
